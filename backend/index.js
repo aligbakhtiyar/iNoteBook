e1943c993c0eb4a6b266e32009b40ae9e41c6ba6
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2022-10-26 21:18:02 +0530

    Fetching all notes & add note

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 //available routes
 app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('/routes/notes'))
+app.use('/api/notes', require('./routes/notes'))
 
 
 // app.get('/', (req, res) => {
